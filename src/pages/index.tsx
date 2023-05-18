e76c85522461b1a8cc749d80fae8b23756080a08
mohammad_saadati <saadati.m.b@gmail.com>
2023-05-18 15:54:32 +0430

    working getstatiprops

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,12 @@
-export default function Home({ data: [] }) {
-  return <div>data</div>;
+export default function Home({ data }) {
+  console.log(data.photos[0].id)
+  return <div>{data.photos[0].id}</div>;
 }
 
 export async function getStaticProps() {
-  const data = await fetch("api/posts");
+  const res = await fetch("http://localhost:3000/api/home");
+
+  const data = await res.json();
 
   return {
     props: { data },
