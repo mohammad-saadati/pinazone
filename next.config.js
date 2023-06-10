6337311be2d72135631803b6e1686b68b6de890a
mohammad_saadati <saadati.m.b@gmail.com>
2023-06-10 22:31:03 +0330

    config

--- next.config.js
+++ next.config.js
@@ -1,8 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  pageExtensions: ["jsx", "js", "tsx", "ts"], // This line is optional and tells Next.js which file extensions to look for in the pages directory
   // Set the page directory to src/pages
-  pagesDir: "src/pages",
 };
 
 module.exports = nextConfig;
