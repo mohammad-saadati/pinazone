15f0bd0b66084007d2591235e87674316e417f4f
mohammad_saadati <saadati.m.b@gmail.com>
2023-07-23 11:25:58 +0330

    counter context

---
+++ src/pages/counter/counter-ctx.tsx
@@ -0,0 +1,34 @@
+import { useState, createContext, useContext } from "react";
+
+const CounterContext = createContext(null);
+
+const CounterContextProvider = ({ children }) => {
+  return (
+    <CounterContext.Provider value={useState(0)}>
+      {children}
+    </CounterContext.Provider>
+  );
+};
+
+const AddoneButton = () => {
+  const [, setCounter] = useContext(CounterContext);
+  return (
+    <div>
+      <button onClick={() => setCounter((v) => v + 1)}>Add one</button>
+    </div>
+  );
+};
+const Counter = () => {
+  const [counter] = useContext(CounterContext);
+
+  return <div>{counter}</div>;
+};
+
+export default function CounterUseState() {
+  return (
+    <CounterContextProvider>
+      <AddoneButton />
+      <Counter />
+    </CounterContextProvider>
+  );
+}
